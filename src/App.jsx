7bc46b1274cb7b14b933114a9c5923bf011afc83
Samuel Echefu <samuelnnechefu@gmail.com>
2025-05-22 01:51:20 -0700

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./css/App.css";
 import Favorite from "./pages/Favourites";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import NavBar from "./componets/NavBar";
 
 function App() {
@@ -9,10 +9,12 @@ function App() {
     <div>
       <NavBar></NavBar>
       <main className="main-content ">
-        <Routes>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/favourites" element={<Favorite />}></Route>
-        </Routes>
+        <BrowserRouter basename="/movieApp">
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/favourites" element={<Favorite />} />
+          </Routes>
+        </BrowserRouter>
       </main>
     </div>
   );
